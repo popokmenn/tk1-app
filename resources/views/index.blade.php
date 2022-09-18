6b757fe7a1f196d24a37721f23721b1e70d9eb3a
RaniTrianiG <ranitrianig@gmail.com>
2022-09-19 02:24:01 +0700

    finishing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,28 +3,32 @@
             <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
         </head>
         <body>
+        <div>
+            <h2 class="textTitle">aplikasi video streaming menggunakan laravel</h2>
+            <h4 class="textTitle">Team 5</h4>
+        </div>
             <form method="post" action="{{ @$existVideo ? Route('update.file', $existVideo['id']) : Route('insert.file') }}" enctype="multipart/form-data">
                 {{ csrf_field() }}
                 <div class="drag-name">
-                    <label>
+                    <label class="textColor">
                         Nama File :
                         <input type="text" name="name" placeholder="input nama file" value="{{ @$existVideo ? $existVideo['name'] : '' }} " />
                     </label>
                 </div>
                 <div class="drag-area">
-                    <label>
+                    <label class="textColor">
                         Menu Upload Video :
+                        <input type="file" name="video" />
                     </label>
-                    <input type="file" name="video" class="margin"/>
                     <p>
                         @if($errors->has('video'))
                             {{ $errors->first('video') }}
                         @endif
                     </p>
-                    <div>
-                        <input type="submit" name="click" />
-                    </div>
                 </div>
+                    <div class ="button">
+                        <input type="submit" name="click" class="styleButton" />
+                    </div>
             </form>
         </body>
 </html>
